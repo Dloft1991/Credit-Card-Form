b93392612be2d7576cebaa1c662e52d6ebe39a1a
Dloft <derek.lofton1@gmail.com>
2020-07-14 10:11:59 -0400

    innerhtml

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-// function MyCard() {
-//     var MyNumber = document.getElementById("card-number").value;
-//     document.getElementById("img-input").innerHTML(MyNumber);
-//     console.log(MyNumber);
-// }
-var MyNumber = document.getElementById("card-number").value;
-$(document).ready(function(){
-    $("input").keydown(function(){
-    //   $("input").css("background-color", "yellow");
-      $("#img-input").append(MyNumber);
-      console.log(MyNumber);
-    });
-    $("input").keyup(function(){
-    //   $("input").css("background-color", "pink");
-    });
-  });
+document.onkeyup = function(event) {
+
+  const CardNumber = event.key;
+
+  
+    document.getElementById("img-input").innerHTML = CardNumber
+    prevetnDefault();;
+  
+
+  
+}
