af8a30c9d9d2c903f8482fc04ce6bc6daf03393c
Dloft <derek.lofton1@gmail.com>
2020-07-15 13:33:48 -0400

    javascript

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 document.onkeyup = function(event) {
 
   const CardNumber = event.key;
+  let MaxLength = 16;
+  var max = parseInt($(this).attr('max'));
+  var min = parseInt($(this).attr('min'));
 
+    if (CardNumber <= MaxLength) {
+      $("#img-input").append(CardNumber);
+      this.value = this.value.replace(/[^0-9\.]/g,'');
+      // document.getElementById("img-input").append(CardNumber);
+    }
+    else if (CardNumber > MaxLength){
+      alert("Too Many Numbers!")
+    }
+    
+    prevetnDefault();
   
-    document.getElementById("img-input").innerHTML = CardNumber
-    prevetnDefault();;
-  
+}
 
+document.onkeyup = function(event) {
+
+  const CardName = event.key;
+  
+      $("#card-name").append(CardNumber);
+      this.value = this.value.replace(/[^a-z\.]/g,'');
+      // document.getElementById("card-name").append(CardName);
+    
+    
+    prevetnDefault();
   
 }
